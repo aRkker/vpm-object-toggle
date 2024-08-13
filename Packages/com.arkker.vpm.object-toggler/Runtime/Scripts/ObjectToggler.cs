593453a26d14fcc2583983ce6e4b2097240222e6
aRkker <arkkerx@gmail.com>
2024-08-13 23:24:48 +0300

    0.0.1

---
+++ Packages/com.arkker.vpm.object-toggler/Runtime/Scripts/ObjectToggler.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class ObjectToggler : UdonSharpBehaviour
+{
+    [Header("List of objects to toggle")]
+    public GameObject[] objects;
+
+    [Header("Reference to the checkbox")]
+    public Toggle checkbox;
+
+    [NonSerialized, UdonSynced] public bool _isOn = true;
+
+
+    void Start()
+    {
+
+    }
+
+    public override void OnDeserialization()
+    {
+        foreach (GameObject obj in objects)
+        {
+            obj.SetActive(_isOn);
+        }
+
+        if (checkbox != null)
+        {
+            checkbox.isOn = _isOn;
+        }
+    }
+
+    public void ToggleState()
+    {
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        _isOn = !_isOn;
+        foreach (GameObject obj in objects)
+        {
+            obj.SetActive(_isOn);
+        }
+
+        if (checkbox != null)
+        {
+            checkbox.isOn = _isOn;
+        }
+
+        RequestSerialization();
+    }
+}
